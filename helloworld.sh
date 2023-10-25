57d5d2dc9f945511953172de44c9dd5edbc7b391
Thierry Tran <anhtuan1996tran@gmail.com>
2023-10-24 17:49:34 -0700

    adding hello.sh script

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Thierry Tran
+# Date of latest revision:      10/24/2023
+# Purpose:                      Ops210d10-challenges 02
+
+# Declaration of variables
+entryText = "Hello World"
+entryText2= "I'm working perfectly."
+
+# Declaration of functions
+
+
+# Main
+echo $entryText
+echo $entryText2
+
+
+# End
