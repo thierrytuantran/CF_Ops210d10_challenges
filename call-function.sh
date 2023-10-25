398250af37ade0273f827230dd437e31b7457307
Thierry Tran <anhtuan1996tran@gmail.com>
2023-10-25 14:13:44 -0700

    login_history script

---
+++ call-function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script Name:                  Login_history
+# Author:                       Thierry Tran
+# Date of latest revision:      10/25/2023
+# Purpose:                      Ops210d10-challenges_03
+
+# Declaration of variables
+
+
+# Declaration of functions
+print_login_history() {
+    last
+    echo "This is the login history"
+}
+
+# Main
+print_login_history
+print_login_history
+print_login_history
+
+
+# End
