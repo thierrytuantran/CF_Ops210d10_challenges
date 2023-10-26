05ad47d4e3eaa3eacafc251040d11559c447ca71
Thierry Tran <anhtuan1996tran@gmail.com>
2023-10-26 11:10:55 -0700

    Creat direcoty.sh script

---
+++ directory.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Thierry Tran
+# Date of latest revision:      10/26/2023
+# Purpose:                      Ops210d10-challenges_04
+
+# Declaration of variables
+
+# Create four directories
+mkdir dir1 dir2 dir3 dir4
+
+# Load directories path into an array
+directories=("dir1" "dir2" "dir3" "dir4")
+
+# Creat the new file
+touch dir1/file1.txt
+touch dir2/file2.txt
+touch dir3/file3.txt
+touch dir4/file4.txt
+# Declaration of functions
+
+
+# Main
+
+# End
